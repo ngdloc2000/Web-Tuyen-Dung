855709c91416786cbfd643006eb64118e9b86d62
ngdloc2000 <ngdinhloc2000@gmail.com>
2022-07-31 17:04:31 +0700

    first commit

---
+++ src/main/java/com/example/web_tuyen_dung/WebTuyenDungApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_tuyen_dung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTuyenDungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebTuyenDungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/web_tuyen_dung/WebTuyenDungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_tuyen_dung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTuyenDungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
