6f8b3c25d478d1dbfd7aaf169254e312422e04fb
ngdloc2000 <ngdinhloc2000@gmail.com>
2022-08-04 23:02:33 +0700

    setting email

---
+++ src/main/java/com/example/web_tuyen_dung/common/constants/MailConstant.java
@@ -0,0 +1,5 @@
+package com.example.web_tuyen_dung.common.constants;
+
+public class MailConstant {
+    public static final String SENDER = "locnd@hblab.vn";
+}
---
+++ src/main/java/com/example/web_tuyen_dung/config/MailConfiguration.java
@@ -0,0 +1,31 @@
+package com.example.web_tuyen_dung.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfiguration {
+
+    @Bean
+    public JavaMailSender getMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("locnd@hblab.vn");
+        mailSender.setPassword("ngdloc2000");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        mailSender.setJavaMailProperties(props);
+        return mailSender;
+    }
+}
